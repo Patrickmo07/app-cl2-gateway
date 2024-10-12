232a5f2b33b6214611a4c54a168efe9ce025289a
Patrickmo07 <patrick.3.mo@gmail.com>
2024-10-12 04:29:38 -0500

    GATEWAY

---
+++ src/main/java/pe/edu/cibertec/app_cl2_gateway/AppCl2GatewayApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_cl2_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCl2GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCl2GatewayApplication.class, args);
+	}
+
+}
